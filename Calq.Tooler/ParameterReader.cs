c23b186398e1cc77fc893ae03f86b2004db4c176
greg-chuchro <gchuchro@acm.org>
2021-09-19 22:02:33 +0900

    #1 command execution

---
+++ Calq.Tooler/ParameterReader.cs
@@ -0,0 +1,33 @@
+﻿using Ghbvft6.Calq.Options;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Calq.Tooler {
+    public class ParameterReader : ReaderBase {
+
+        IEnumerable<ParameterInfo> parameters;
+
+        public ParameterReader(IEnumerable<ParameterInfo> parameters) : base() {
+            this.parameters = parameters;
+        }
+
+        protected override string GetOptionName(char option) {
+            foreach (var param in parameters) {
+                if (param.Name[0] == option) { // TODO when null?
+                    return param.Name;
+                }
+            }
+            throw new Exception($"option doesn't exist: {option}");
+        }
+
+        protected override Type GetOptionType(string option) {
+            foreach (var param in parameters) {
+                if (param.Name == option) {
+                    return param.ParameterType;
+                }
+            }
+            throw new Exception($"option doesn't exist: {option}");
+        }
+    }
+}
